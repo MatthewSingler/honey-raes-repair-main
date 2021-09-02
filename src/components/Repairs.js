3ed04b73ec2e414cbe89885225ae04bf12462f72
Matthew Singler <matthew.singler@gmail.com>
2021-09-02 13:45:43 -0500

    Added a logout link in the NavBar.js

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,13 +1,38 @@
 import React from "react"
+import { Route } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./Nav/NavBar"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import { Redirect } from "react-router"
 import "./Repairs.css"
+
+
 export const Repairs = () => {
     return (
         <>
-            <NavBar />
-            <h1>Honey Rae's Repair Shop</h1>
-            <ApplicationViews />
-            </>
-    )
+            <Route
+                render={() => {
+                    if (localStorage.getItem("honey_customer")) {
+                        return (
+                            <>
+                                <NavBar />
+                                <h1>Honey Rae's Repairs</h1>
+                                <ApplicationViews />
+                            </>
+                        );
+                    } else {
+                        return <Redirect to="/login" />;
+                    }
+                }}
+            />
+
+            <Route path="/login">
+                <Login />
+            </Route>
+            <Route path="/register">
+                <Register />
+            </Route>
+        </>
+    );
 }
